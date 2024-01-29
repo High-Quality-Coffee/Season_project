793a07f11fa6e798eb9e6757c7848f042dce5238
highqc24 <gw2000sk@korea.ac.kr>
2024-01-29 17:44:08 +0900

    feat : task/admin/assign 회원정보 db저장 실패 이름,이메일,전화번호,면접날짜 db에 저장하려다 방황하며 실패해버림 하하

--- src/app/page.task.admin.assign/api.py
+++ src/app/page.task.admin.assign/api.py
@@ -1,3 +1,21 @@
-INSERT INTO user_info
-  (name,email,phone)
-  VALUES ('박규원', 'gw2000sk@naver.com', 01058358923);
+import re
+
+userdb=wiz.model("orm").use("user")
+
+db=struct.db("user_info")
+
+def save():
+    user={}
+    user["id"]=wiz.request.query('id')
+    user["name"]=wiz.request.query('name')
+    user["email"]=wiz.request.query('email')
+    user["phone"]=wiz.request.query('phone')
+    user["interview"]=wiz.request.query('interview')
+
+    db.update(userdb)
+
+    
+
+    return wiz.response.status(200, True)
+    
+
