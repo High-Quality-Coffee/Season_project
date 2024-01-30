6736ebe55fcf04c3f7c11fadcf9d15e50cfe0f4f
highqc24 <gw2000sk@korea.ac.kr>
2024-01-30 17:48:52 +0900

    feat : user.py db구조 변경 테스트로 db 구조 변경해서 db 저장 확인

--- src/model/db/user.py
+++ src/model/db/user.py
@@ -1,12 +1,27 @@
+# import peewee as pw
+# base = wiz.model("orm_base")
+
+# class Model(base):
+#     class Meta:
+#         db_table = 'user'
+
+#     id = pw.CharField(max_length=32)
+#     name = pw.CharField(max_length=32)
+#     email = pw.CharField(unique=True, max_length=192)
+#     role = pw.CharField(index=True, max_length=16, default='user')
+#     password = base.PasswordField()
+
+
 import peewee as pw
 base = wiz.model("orm_base")
 
 class Model(base):
     class Meta:
-        db_table = 'user'
+        db_table = 'user_info'
 
     id = pw.CharField(max_length=32)
     name = pw.CharField(max_length=32)
     email = pw.CharField(unique=True, max_length=192)
     role = pw.CharField(index=True, max_length=16, default='user')
     password = base.PasswordField()
+
