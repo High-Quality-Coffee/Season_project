6a8c2b0b0daa84495473bd375d2d51b787ea4ac6
highqc24 <gw2000sk@korea.ac.kr>
2024-02-07 16:00:25 +0900

    feat : season.py 500에러 해결 문제가 되는 코드 주석처리함 왜 문제가 되는지는 잘 모르겠음

--- src/app/page.task.admin.assign/api.py
+++ src/app/page.task.admin.assign/api.py
@@ -1,6 +1,10 @@
 import re
+import season
 import datetime
-from datetime import datetime, timedelta
+import json
+import os
+
+
 
 #user_info db 사용
 userdb=wiz.model("orm").use("user_info")
@@ -18,18 +22,7 @@ def assign():
     user["center"]=wiz.request.query('center', True)
     user["password"]='season'+endPhoneNum
     user["role"]="user"
-    
-    date_string = user["interview"]
-    given_datetime = strptime(date_string, "%Y-%m-%d %H:%M:%S")
-    midnight_one_day_ago = given_datetime.replace(hour=0, minute=0, second=0, microsecond=0) - timedelta(days=1)
-    result_string = midnight_one_day_ago.strftime("%Y-%m-%d %H:%M:%S")
-    print(result_string)
 
-    user['duedate']=result_string
-    user["center"]=wiz.request.query('center', True)
-    user["password"]='season'+endPhoneNum
-    user["role"]="user"
-    
     userdb.insert(user)
 
     return wiz.response.status(200, True)
